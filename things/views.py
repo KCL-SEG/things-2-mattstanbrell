4dd1e12527ba810a0df0b343f9c4c93a87196d6c
Matt Stanbrell <mattstanbrell@gmail.com>
2022-11-19 17:27:24 +0000

    Change name to ThingForm

--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .forms import SignUpForm
+from .forms import ThingForm
 
 def home(request):
-    form = SignUpForm()
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
